2bf7bad0c9406676d1224466548b0d580307b7fa
Aaron Barany <akb825@gmail.com>
2018-10-24 22:39:50 -0700

    SDL is only shared or static.
    Use static build except for Android, which uses shared.

--- scripts/sdl-build.sh
+++ scripts/sdl-build.sh
@@ -9,7 +9,13 @@ NAME="SDL2-$VERSION"
 curl "http://libsdl.org/release/$NAME.tar.gz" --output "$NAME.tar.gz"
 tar xzf "$NAME.tar.gz"
 
+if [ $PLATFORM = android ]; then
+	patch -p1 -d "$NAME" -i "$DIR/sdl-android-support.patch"
+	FLAGS="$FLAGS -DSDL_STATIC=OFF"
+else
+	FLAGS="$FLAGS -DSDL_SHARED=OFF"
+fi
+
 pushd "$NAME" > /dev/null
-patch -p1 -i "$DIR/sdl-android-support.patch"
 "$DIR/$PLATFORM-build.sh" $FLAGS $@
 popd > /dev/null
